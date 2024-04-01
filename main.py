20c31d16383c40e2253021af271679ab37b11d91
AndriusGelezevicius <andriusgelezevicius@gmail.com>
2024-04-01 13:14:15 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,50 @@
+import pygame
+
+pygame.init()
+# Creating a screen
+screen = pygame.display.set_mode((800,600))
+
+# title, icon
+pygame.display.set_caption("Road Fighter")
+icon = pygame.image.load("images\icon.png")
+pygame.display.set_icon(icon)
+
+# Player
+playerImg = pygame.image.load("images\player.png")
+playerX = 370
+playerY = 480
+playerX_change = 0
+
+
+def player(x,y):
+    screen.blit(playerImg, (x,y))
+
+
+
+
+running = True
+while running:
+    #Clear the screen
+    screen.fill((0,0,0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.3
+                print("LEFT")
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.3
+                print("RIGHT")
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+    playerX += playerX_change
+    player(playerX, playerY)
+
+    #Update the display
+    pygame.display.update()
+
+
+
+
