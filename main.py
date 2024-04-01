a0e9dd16b67ff50ccb776729f1e10d51e69b8517
AndriusGelezevicius <andriusgelezevicius@gmail.com>
2024-04-01 13:19:16 +0300

    add arrows up and down functionality

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ playerImg = pygame.image.load("images\player.png")
 playerX = 370
 playerY = 480
 playerX_change = 0
+playerY_change = 0
 
 
 def player(x,y):
@@ -36,10 +37,21 @@ while running:
             if event.key == pygame.K_RIGHT:
                 playerX_change = 0.3
                 print("RIGHT")
+            if event.key == pygame.K_UP:
+                playerY_change = -0.3
+                print("UP")
+            if event.key == pygame.K_DOWN:
+                playerY_change = 0.3
+                print("DOWN")
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
+            if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                playerY_change = 0
+
+
     playerX += playerX_change
+    playerY += playerY_change
     player(playerX, playerY)
 
     #Update the display
